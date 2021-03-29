7c43bf73bd444fe02c49d9441728f71e013b0e4f
Jawsbreaker <evansrichardmichael@gmail.com>
2021-03-29 01:13:56 -0600

    port fix heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const PORT = process.env.PORT || 3001
 const app = express();
 
 
@@ -58,6 +58,6 @@ app.get('/api/animals', (req, res) => {
 
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`)
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
 })
